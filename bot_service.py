e535f0f0b1977b1796490a23da578160f2245b54
ivan-perhun <ivanalexwork@gmai.com>
2023-03-25 23:48:54 +0300

    replace all code to aiogram lib

--- bot_service.py
+++ bot_service.py
@@ -1,28 +1,9 @@
 #%%
-import os
-import uuid
-
 from db_utils.user_db_manager import UserDatabaseManager
 from db_utils.models.users import User
-import dotenv
 
-dotenv.load_dotenv()
+db_path = 'db_utils/db/test.db'
+db_manager = UserDatabaseManager.get_instance(db_path)
 
-tg_api_key = os.getenv('TELEGRAM_API_KEY')
-open_ai_password = os.getenv('OPEN_AI_PASSWORD')
-open_ai_login = os.getenv('OPEN_AI_EMAIL')
-open_ai_api_key = os.getenv('OPEN_AI_API_KEY')
-
-
-users_db = UserDatabaseManager.get_instance('db_utils/db/users.db')
-
-user_2 = User(id=str(uuid.uuid4()), tg_api_key=tg_api_key, open_ai_password=open_ai_password, open_ai_login=open_ai_login, open_ai_api_key=open_ai_api_key, active=True)
-user_3 = User(id=str(uuid.uuid4()), tg_api_key=tg_api_key, open_ai_password=open_ai_password, open_ai_login=open_ai_login, open_ai_api_key=open_ai_api_key, active=True)
 
 #%%
-from bot.bot import MyChatBot
-
-all_active_users = users_db.get_active_users()
-all_active_users
-
-
--- config.py
+++ config.py
@@ -1,8 +1,21 @@
 import os
+from configparser import ConfigParser
 
-class Config:
-    # Токен Telegram Bot API
-    TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
+class ConfigManager:
+    def __init__(self, config_file_path='config.ini'):
+        self.config_file_path = config_file_path
+        self.config = ConfigParser()
+        self.config.read(self.config_file_path)
+
+    def get(self, section='default', key=None):
+        if key is None:
+            return self.config.items(section)
+        else:
+            return self.config.get(section, key)
+        
+    def get_tg_bot_token(self):
+        return self.get('default', 'TELEGRAM_BOT_TOKEN')
     
-    # API-ключ OpenAI
-    OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+    def get_openai_api_key(self):
+        return self.get('default', 'OPEN_AI_API_KEY')
+    
